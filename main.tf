a199cd35c982e5d5cbbcf5e4f12acf92a9708f3e
mc-b <marcel.bernet@ch-open.ch>
2022-04-25 08:36:34 +0200

    Abhandlung host_no bzw. Entfernung dessen

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ data "maas_vm_hosts" "vm-hosts" {
 resource "maas_vm_instance" "vm" {
   count = length(data.maas_vm_hosts.vm-hosts.no) * var.vm_per_host
   kvm_no = data.maas_vm_hosts.vm-hosts.no[count.index % length(data.maas_vm_hosts.vm-hosts.no)]
-  hostname = "${var.module}-${format("%02d", count.index + var.vm_offset)}-${terraform.workspace}"
+  hostname = "${split("-", var.module)[0]}-${format("%02d", count.index + var.vm_offset)}-${terraform.workspace}"
   
   cpu_count = var.cores
   memory = var.memory * 1024
